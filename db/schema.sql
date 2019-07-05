9ed982e5d66816345350961d5cfdfc3c10ff766c
Damian Sanchez <damiancola23@gmail.com>
2019-07-05 10:40:08 -0700

    new download

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
+
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT,
+  burger_name varchar(30) NOT NULL,
+  devoured boolean NOT NULL,
+  PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger",  false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Mac and Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mc Fatty", false);
